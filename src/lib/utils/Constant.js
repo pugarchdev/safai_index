6bdc319beee47a2baabdc2b71a71adad342f13cd
pugarchdev <pugarchdev@gmail.com>
2026-01-01 23:13:06 +0530

    Update Constant.js

--- src/lib/utils/Constant.js
+++ src/lib/utils/Constant.js
@@ -5,7 +5,7 @@
 // const API_BASE_URL = "http://localhost:8000/api";
 // const API_BASE_URL = "https://saaf-ai-backend.onrender.com/api";
 // const API_BASE_URL = "https://safai-ai-651690228479.asia-south1.run.app/api"
-const API_BASE_URL = "https://saaf-ai-backend.vercel.app/api"
+const API_BASE_URL = "https://saaf-backend.vercel.app/api"
 
 
 export default API_BASE_URL;
