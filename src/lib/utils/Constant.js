04db49464b912612981f4802a8f93579dba59581
pugarchdev <pugarchdev@gmail.com>
2026-01-01 23:44:40 +0530

    Update Constant.js

--- src/lib/utils/Constant.js
+++ src/lib/utils/Constant.js
@@ -6,6 +6,8 @@
 // const API_BASE_URL = "https://saaf-ai-backend.onrender.com/api";
 // const API_BASE_URL = "https://safai-ai-651690228479.asia-south1.run.app/api"
 const API_BASE_URL = "https://saaf-backend.vercel.app/api"
+// the commit does not take affect redeploying it with comment
+const name = "demo string "
 
 
 export default API_BASE_URL;
